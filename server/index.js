103298c499cf3e7ca211d439be30da4bf5aa1641
antonnprince <antonprince95@gmail.com>
2024-07-06 10:22:59 +0530

    basic communication through socket

--- server/index.js
+++ server/index.js
@@ -5,13 +5,20 @@ const httpServer = createServer()
 
 const socket = new Server(httpServer, {
     cors:{
-        origin:"http://localhost:5500"
+        origin:"http://127.0.0.1:5500"
     }
 })
 
 socket.on('connect', (socket)=>{
-    console.log(socket)
+    // console.log(socket)
+
+    socket.emit("message","HELLOO")
+    socket.on("message",(data)=>{
+        console.log(data)
+    })
 })
+
+
 
 httpServer.listen(3000, ()=>{
     console.log("Server is connected")
